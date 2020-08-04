f5b395187c640bc4e554789c77f0abc7fb22c961
cgwelcome <charlesgao.c@gmail.com>
2020-08-03 23:25:12 -0400

    Refactor to be compatible with stow

---
+++ fzf/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/charles/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/charles/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/charles/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/charles/.fzf/shell/key-bindings.zsh"
